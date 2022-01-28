bdcb1b0b6cad05c1ef2160b410834576230b7ae3
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2022-01-28 22:36:58 +0000

    Unity Integration Setup

---
+++ Assets/Scripts/Player Movement/PlayerControls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get left button, move left
+
+        //Get right button, move right
+
+        //Get Jump button, jump
+
+    }
+}
